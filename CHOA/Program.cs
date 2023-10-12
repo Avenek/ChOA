876c613f1dfd1b57b3db909353204cae23f32552
Avenek <j.machnik2002@gmail.com>
2023-10-12 17:37:33 +0200

    Remove comments

--- CHOA/Program.cs
+++ CHOA/Program.cs
@@ -4,27 +4,20 @@
     {
         static void Main()
         {
-            //for(int i = 0; i< 10; i++)
-            //{
-            //    IFitnessFunction fitnessFunction = new Rastrigin();
-            //    ChimpOptimizationAlgorithm CHOA = new ChimpOptimizationAlgorithm(50, 5, 1000, fitnessFunction);
-            //    CHOA.Solve();
-            //    Console.WriteLine($"Wektor: {String.Join("; ", CHOA.XBest)}\nWartość: {CHOA.FBest}");
-            //}
-
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    IFitnessFunction fitnessFunction = new Rosenbrock();
-            //    ChimpOptimizationAlgorithm CHOA = new ChimpOptimizationAlgorithm(50, 5, 500, fitnessFunction);
-            //    CHOA.Solve();
-            //    Console.WriteLine($"Wektor: {String.Join("; ", CHOA.XBest)}\nWartość: {CHOA.FBest}");
-            //}
-
-            double x = 0.7;
-            for(int i = 0; i <100; i++)
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(x);
-                x = x * x - 1;
+                IFitnessFunction fitnessFunction = new Rastrigin();
+                ChimpOptimizationAlgorithm CHOA = new ChimpOptimizationAlgorithm(50, 5, 1000, fitnessFunction);
+                CHOA.Solve();
+                Console.WriteLine($"Wektor: {String.Join("; ", CHOA.XBest)}\nWartość: {CHOA.FBest}");
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                IFitnessFunction fitnessFunction = new Rosenbrock();
+                ChimpOptimizationAlgorithm CHOA = new ChimpOptimizationAlgorithm(50, 5, 500, fitnessFunction);
+                CHOA.Solve();
+                Console.WriteLine($"Wektor: {String.Join("; ", CHOA.XBest)}\nWartość: {CHOA.FBest}");
             }
         }
     }
